29b974acb0dc48c4654c26607f0cb2f533a40e1b
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-06-18 00:50:21 +0300

    update path and tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
-import { BaseLayout, NftList } from "../components";
+import { BaseLayout, NftList } from "@ui";
 import nfts from "../content/meta.json";
-import { NftMetadata } from "../types/nft";
+import { NftMetadata } from "@_types/nft";
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
         </div>
         <div className="relative">
           <div className="text-center">
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-              Amazing Creatures NFTs
-            </h2>
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Creatures NFTs</h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
               Mint a NFT to get unlimited ownership forever!
             </p>
