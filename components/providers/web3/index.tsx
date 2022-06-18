5bf638b8063746563040c7e8f528952e9a7639a0
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-06-18 23:20:16 +0300

    web3 provider added

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,6 +1,9 @@
-import { createContext, FunctionComponent, useContext, useState } from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { createContext, FunctionComponent, useContext, useEffect, useState } from "react";
+import { createDefaultState, Web3State } from "./utils";
+import { ethers } from "ethers";
 
-const Web3Context = createContext<any>(null);
+const Web3Context = createContext<Web3State>(createDefaultState());
 
 // type props
 interface Web3ProviderProps {
@@ -8,7 +11,17 @@ interface Web3ProviderProps {
 }
 
 const Web3Provider: FunctionComponent<Web3ProviderProps> = ({ children }) => {
-  const [web3, setWeb3] = useState({ test: "Hello Provider!" });
+  const [web3, setWeb3] = useState<Web3State>(createDefaultState());
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+
+    function initWeb3() {
+      setWeb3({ isLoading: false, contract: null, ethereum: window.ethereum, provider });
+    }
+
+    initWeb3();
+  }, []);
 
   return <Web3Context.Provider value={web3}>{children}</Web3Context.Provider>;
 };
