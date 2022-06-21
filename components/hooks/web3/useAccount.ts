8605bd445373845573c25a5040e2fc3f1270a44c
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-06-21 15:50:34 +0300

    conditional swr

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -6,13 +6,14 @@ type AccountFactory = CryptoHookFactory<string, string>;
 export type UseAccountHook = ReturnType<AccountFactory>;
 
 // deps -> provider, ethereum, contract (web3 state)
-export const hookFactory: AccountFactory = (deps) => (params) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const swrRes = useSWR("web3/useAccount", () => {
-    console.log(deps);
-    console.log(params);
-    return "Test user";
-  });
+export const hookFactory: AccountFactory =
+  ({ provider }) =>
+  (params) => {
+    // conditionally swr block
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const swrRes = useSWR(provider ? "web3/useAccount" : null, () => {
+      return "Test user";
+    });
 
-  return swrRes;
-};
+    return swrRes;
+  };
