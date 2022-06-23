268479daba178fc7be3696f3136ca35c10e5da92
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-06-23 14:17:16 +0300

    Network namae

--- components/hooks/web3/useNetwork.ts
+++ components/hooks/web3/useNetwork.ts
@@ -1,6 +1,17 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
+const NETWORKS: { [k: string]: string } = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Georli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  97: "Binance Smart Chain Test Network",
+  1337: "Ganache",
+};
+
 type UseNetworkResponse = {
   isLoading: boolean;
 };
@@ -15,7 +26,9 @@ export const hookFactory: NetworkHookFactory =
     const { data, isValidating, ...swr } = useSWR(
       provider ? "web3/useNetwork" : null,
       async () => {
-        return "Testing Network";
+        const chainId = (await provider!.getNetwork()).chainId;
+
+        return NETWORKS[chainId];
       },
       {
         revalidateOnFocus: false,
