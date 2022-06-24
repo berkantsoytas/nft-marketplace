f78a3e3462e8574609f05702fc13f5a56e84cce1
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-06-24 09:37:15 +0300

    nft purchase was added to the contract and tests were carried out

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -60,6 +60,20 @@ contract NftMarket is ERC721URIStorage{
     return newTokenId;
   }
 
+  function buyNft (uint tokenId) public payable {
+    uint256 price = _idToNftItem[tokenId].price;
+    address owner = ERC721.ownerOf(tokenId);
+
+    require(msg.sender != owner, "NFT Market: You already own this NFT");
+    require(msg.value == price, "NFT Market: Please submit the asking price");
+
+    _idToNftItem[tokenId].isListed = false;
+    _listedItems.decrement();
+
+    _transfer(owner, msg.sender, tokenId);
+    payable(owner).transfer(msg.value);
+  }
+
   function createNftItem(uint256 tokenId, uint256 price) private {
     require(price > 0, "NFT Market:  Price must be at least 1 wei");
   
