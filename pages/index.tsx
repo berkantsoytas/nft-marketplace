8000ed666067bcc385cf2ab03dc06035f08486c2
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-06-30 00:56:24 +0300

    useListedNfts hook added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import { BaseLayout, NftList } from "@ui";
-import nfts from "../content/meta.json";
 import { NftMetadata } from "@_types/nft";
-import { useWeb3 } from "@providers/web3";
+import { useListedNfts } from "@hooks/web3";
 
 const Home: NextPage = () => {
-  const { provider, contract } = useWeb3();
-
+  const { nfts } = useListedNfts();
+  console.log(nfts.data);
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -23,7 +22,7 @@ const Home: NextPage = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList nfts={nfts as NftMetadata[]} />
+          <NftList nfts={nfts.data} />
         </div>
       </div>
     </BaseLayout>
